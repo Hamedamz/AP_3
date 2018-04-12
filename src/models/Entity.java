26a821b454ba6df670cf3e9e2284e990095db09f
Hamed <amzhamed@gmail.com>
2018-04-12 16:42:16 +0430

    Position model added

--- src/models/Entity.java
+++ src/models/Entity.java
@@ -1,31 +1,21 @@
 package models;
 
 public class Entity {
-    protected int x;
-    protected int y;
+    protected Position position;
 
     public Entity() {
     }
 
     public Entity(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
 
     }
 
-    public void setY(int y) {
+    public Position getPosition() {
+        return position;
+    }
 
+    public void setPosition(Position position) {
+        this.position = position;
     }
 
     public void move(int x, int y) {
