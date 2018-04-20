603c36a0ac67473911922b3db3bc47fdf57c2384
LWWhite <arshiamoghimi98@gmail.com>
2018-04-20 09:53:18 +0430

    Handled main menu items and changed IOFormat class fields from private to public

--- src/Controller.java
+++ src/Controller.java
@@ -2,56 +2,99 @@ import models.World;
 
 public class Controller {
 
-    private Viewer viewer;
-    private World world;
+    private static Viewer viewer = new Viewer();
+    private static World world = new World();
 
     public static void main(String[] args) {
+        String input = viewer.getInput();
+        //unknown end command(?)
+        while (true) {
+            if (input.equals(IOFormats.NEWGAME_FORMAT)){
+                newGame();
+            }
+            else if (input.equals(IOFormats.LOAD_PATH_FORMAT)) {
+                String path = input.split("\\s")[1];
+                try {
+                    loadGame(path);
+                }
+                catch (Exception e) {
+                    viewer.printError(IOFormats.NO_VALID_FILE_ERROR);
+                }
+            }
+            else if (input.equals(IOFormats.SAVE_FORMAT)) {
+                String path = input.split("\\s")[1];
+                String name = input.split("\\s")[2];
+                try {
+                    saveGame(path, name);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (input.equals(IOFormats.TURN_FORMAT)) {
+                int turns = Integer.parseInt(input.split("\\s")[1]);
+                turn(turns);
+            }
+            else if (input.equals(IOFormats.SHOW_BUILDINGS_FORMAT)) {
+                showBuildings();
+            }
+            else if (input.equals(IOFormats.SHOW_RESOURCES_FORMAT)) {
+                showResources();
+            }
+            else if (input.equals(IOFormats.SHOW_MENU_FORMAT)) {
+                viewer.printMessage(IOFormats.VILLAGE_PRESENCE_FORMAT);
+            }
+            else if (input.equals(IOFormats.WHERE_AM_I_FORMAT)) {
+                whereAmI();
+            }
+            else if (input.equals(IOFormats.ATTACK_FORMAT)) {
+                attack();
+            }
+        }
+    }
+
+    public static void newGame() {}
+
+    public static void loadGame(String path) {
+    }
+
+    public static void saveGame(String path, String name) {
 
     }
 
-    public void newGame() {}
-
-    public boolean loadGame(String path) {
-        return true;
-    }
-
-    public boolean saveGame(String path, String name) {
-        return true;
-    }
-
-    public void turn(int n) {
+    public static void turn(int n) {
 
     }
 
-    public void showBuildings() {
+    public static void showBuildings() {
+        //TODO buildings menus should be handled here
+    }
+
+    public static void showResources() {
 
     }
 
-    public void showResources() {
+    public static void showInfo() {
 
     }
 
-    public void showInfo() {
+    public static void upgrade() {
 
     }
 
-    public void upgrade() {
+    public static void back() {
 
     }
 
-    public void back() {
+    public static void showMenu() {
 
     }
 
-    public void showMenu() {
+    public static void whereAmI() {
 
     }
 
-    public void whereAmI() {
-
-    }
-
-    public void attack() {
+    public static void attack() {
 
     }
 
