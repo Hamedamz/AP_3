48277e7856c1610e69fbb9dc9e0d8e04512729d4
Hamed <amzhamed@gmail.com>
2018-04-25 12:39:15 +0430

    added Item interface and Button for Menu

--- src/models/Menu.java
+++ src/models/Menu.java
@@ -1,13 +1,47 @@
 package models;
 
+import interfaces.Item;
+
 import java.util.ArrayList;
 
-public class Menu {
+public class Menu implements Item {
     private String name;
-    private ArrayList<MenuItem> items;
+    private ArrayList<Item> items;
     private Menu parent;
 
-    public ArrayList<MenuItem> getItems() {
+    public ArrayList<Item> getItems() {
         return items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addItem (Item item) {
+        items.add(item);
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
+
+abstract class Button implements Item {
+    private String label;
+
+    public Button(String name) {
+        this.label = name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
+
+class MenuBuilder {
+    public Menu BuildMainMenu() {
+        Menu menu = new Menu();
+        return menu;
     }
 }
--- src/models/MenuItem.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public class MenuItem {
-    private String name;
-}
