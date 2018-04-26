7f23b41cf793c1349eb5a773d58685a12928c3b3
LWWhite <arshiamoghimi98@gmail.com>
2018-04-26 09:00:51 +0430

    added JsonInterpreter class.

---
+++ src/controllers/JsonInterpreter.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import models.GameLogic.Village;
+
+public class JsonInterpreter {
+    private static Gson gson = new GsonBuilder().create();
+
+    public static void saveVillage(Village village) {
+
+    }
+}
