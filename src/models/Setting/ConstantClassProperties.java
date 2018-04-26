4585ef60bec992fa2faf623d3cec9f5d459fa792
SoroushVT <svafaiet@gmail.com>
2018-04-26 19:33:47 +0430

    ConstantClassProperties bugs fixed

--- src/models/Setting/ConstantClassProperties.java
+++ src/models/Setting/ConstantClassProperties.java
@@ -2,14 +2,17 @@ package models.Setting;
 
 import javax.imageio.IIOException;
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public abstract class ConstantClassProperties {
-    private static Map<String, Integer> classProperties;
+    private static Map<String, Number> classProperties;
 
-    public static final String CONSTANTS_SAVE_LOCATION = "class properties.txt";
+    private static final String CONSTANTS_SAVE_LOCATION = "class properties.txt";
+    private static final String IGNORE_REGEX = "#.*";
+    private static final String PROPERTY_FORMAT = "((\\D(_\\d+)?)|\\s)+:\\s+\\d+(\\.\\d+)?";
 
     public static void loadConstants() {
         classProperties = new HashMap<>();
@@ -17,6 +20,21 @@ public abstract class ConstantClassProperties {
             File constantFile = new File(CONSTANTS_SAVE_LOCATION);
             InputStream stream = new FileInputStream(constantFile);
             Scanner scanner = new Scanner(stream);
+            while (scanner.hasNext()) {
+                String newLine = scanner.nextLine();
+                if (!newLine.matches(IGNORE_REGEX)) {
+                    newLine = newLine.replaceAll("\\s+", " ");
+                    if(newLine.matches(PROPERTY_FORMAT)) {
+                        if(newLine.charAt(0) == ' ') {
+                            newLine = newLine.substring(1);
+                        }
+                        String[] mapParts = newLine.split("\\s*:\\s*");
+                        if(mapParts.length == 2) {
+                            classProperties.put(mapParts[0], Double.parseDouble(mapParts[1]));
+                        }
+                    }
+                }
+            }
             String key = scanner.next();
             int value = scanner.nextInt();
             classProperties.put(key, value);
@@ -25,7 +43,7 @@ public abstract class ConstantClassProperties {
         }
     }
 
-    public static int getFromDictionary(String value) {
+    public static Number getFromDictionary(String value) {
         return classProperties.get(value);
     }
 }
