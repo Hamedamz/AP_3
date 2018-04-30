5cada19ab72febdbf56f4aa3854516d72d04c312
SoroushVT <svafaiet@gmail.com>
2018-04-30 16:50:51 +0430

    village Resources functions implemented

--- src/models/GameLogic/Map.java
+++ src/models/GameLogic/Map.java
@@ -8,23 +8,17 @@ import java.util.ArrayList;
 public class Map {
     // TODO: 4/12/2018 add map size to dic
 
-    private ArrayList<Entity> entities;
     private ArrayList<Building> buildings;
 
 
     public Map() {
-        entities = new ArrayList<>();
         buildings = new ArrayList<>();
     }
 
     public void addNewBuilding(Building building) {
-        addNewEntity(building);
         buildings.add(building);
     }
 
-    public void addNewEntity(Entity entity) {
-        entities.add(entity);
-    }
     public Map clone() {
 
     }
