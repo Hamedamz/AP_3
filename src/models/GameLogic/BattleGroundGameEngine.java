b2f30da96159de0e9a219d6beaae05ebc51cc4d5
LWWhite <arshiamoghimi98@gmail.com>
2018-05-06 22:05:33 +0430

    edited updateBuildingTarget method

--- src/models/GameLogic/BattleGroundGameEngine.java
+++ src/models/GameLogic/BattleGroundGameEngine.java
@@ -1,9 +1,9 @@
 package models.GameLogic;
 
+import interfaces.Attacker;
 import interfaces.Destroyable;
 import models.GameLogic.Entities.Buildings.Building;
 import models.GameLogic.Entities.Buildings.DefensiveBuilding;
-import models.GameLogic.Entities.Defender;
 import models.GameLogic.Entities.Troop.AttackerTroop;
 import models.GameLogic.Entities.Troop.Troop;
 import models.GameLogic.Exceptions.NoTargetFoundException;
@@ -27,8 +27,8 @@ public class BattleGroundGameEngine {
         isGameFinished = battleGround.isGameFinished();
     }
 
-    private void updateTroopTarget() {
-        ArrayList<Destroyable> listOfDefensiveUnits = new ArrayList<>(battleGround.getVillage().getMap().getBuildings());
+    public void updateTroopTarget() {
+        ArrayList<Destroyable> listOfDefensiveUnits = new ArrayList<>(battleGround.getMap().getBuildings());
         for (Troop troop : battleGround.getTroops()) {
             if (troop instanceof AttackerTroop) {
                 if (((AttackerTroop) troop).getTarget() == null || ((AttackerTroop) troop).getTarget().isDestroyed()) {
@@ -42,41 +42,21 @@ public class BattleGroundGameEngine {
         }
     }
 
-    private void updateBuildingTarget() {
+    public void updateBuildingTarget() {
+        ArrayList<Destroyable> destroyables = new ArrayList<>(battleGround.getTroops());
         for (Building building : battleGround.getMap().getBuildings()) {
             if (building instanceof DefensiveBuilding) {
-                if (((DefensiveBuilding) building).getTarget() == null || ((DefensiveBuilding) building).getTarget().isDestroyed()) {
-                    ((DefensiveBuilding) building).setTarget(null);// FIXME: 5/5/2018  use attacker setTarget function ArshiaMoghimi
+                if (((Attacker) building).getTarget() == null || ((Attacker) building).getTarget().isDestroyed()) {
+                    try {
+                        ((Attacker) building).setTarget(destroyables);
+                    } catch (NoTargetFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
     }
 
-    private void findMovableEffectPostion() {
-        for(Troop troop : battleGround.getTroops()) {
-            
-            // TODO: 5/6/2018 bfs algorithm which gets a movable and
-            // find an arraylist of Locatables with size of speed and set that in movable
-            troop.move(troop.getPath().get(troop.getSpeed()-1), battleGround.getMap());
-            //first phase only
-        }
 
-    }
 
-    private void performBuildingsEffects() {
-        for (Building building : battleGround.getMap().getBuildings()) {
-            if(building instanceof DefensiveBuilding) {
-                if(!building.isDestroyed() && ((DefensiveBuilding) building).getTarget() != null) {
-                    ((DefensiveBuilding) building).giveDamageTo(((DefensiveBuilding) building).getTarget(),
-                            battleGround.getMap());
-                }
-            }
-
-        }
-    }
-
-    private void performTroopsEffects() {
-        //TODO arshia moghimi
-        //don't forget to think about bounties!
-    }
 }
