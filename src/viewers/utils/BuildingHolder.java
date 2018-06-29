538909fdb9ce22ee2302246fcefb5b1d57dd4d88
Hamed <amzhamed@gmail.com>
2018-06-29 12:41:47 +0430

    ProgressBar improved

--- src/viewers/utils/BuildingHolder.java
+++ src/viewers/utils/BuildingHolder.java
@@ -3,15 +3,21 @@ package viewers.utils;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
-
-
 public class BuildingHolder extends StackPane {
-    ImageView imageView;
-    ProgressBar progressBar;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     public BuildingHolder(ImageView imageView) {
         this.imageView = imageView;
         this.setMaxSize(imageView.getViewport().getWidth(), imageView.getViewport().getHeight());
         this.getChildren().addAll(imageView);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public ProgressBar getProgressBar() {
+        return progressBar;
     }
 }
--- src/viewers/utils/ProgressBar.java
+++ src/viewers/utils/ProgressBar.java
@@ -10,16 +10,32 @@ public class ProgressBar extends StackPane {
     private double percentage;
     private Rectangle bar;
     private Rectangle background;
-
+    private Color color = Color.LIMEGREEN;
 
     public ProgressBar(double width, double height, double percentage) {
         this.width = width;
         this.height = height;
         this.percentage = percentage;
-        bar = new Rectangle(width * percentage, height, Color.SEAGREEN);
+        bar = new Rectangle(width * percentage, height, this.color);
         background = new Rectangle(width + 2, height + 2, Color.BLACK);
         background.setOpacity(0.5);
+        this.setMaxSize(width + 2, height + 2);
+        this.getChildren().addAll(background, bar);
     }
 
+    public void update(double percentage) {
+        this.percentage = percentage;
+        bar.setWidth(width * this.percentage);
+        if (this.percentage == 1) {
+            this.setFinished();
+        }
+    }
 
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    private void setFinished() {
+        this.setVisible(false);
+    }
 }
