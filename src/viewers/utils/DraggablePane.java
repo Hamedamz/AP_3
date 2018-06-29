43cbe89e4019ccc5dba452fa78e4ffe597d6f1a3
SoroushVT <svafaiet@gmail.com>
2018-06-30 01:16:29 +0430

    zoom around cursor ability added to zoomable pane

--- src/viewers/utils/DraggablePane.java
+++ src/viewers/utils/DraggablePane.java
@@ -1,13 +1,15 @@
 package viewers.utils;
 
-import javafx.geometry.Bounds;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Translate;
+import javafx.util.Pair;
 
 import static viewers.utils.Const.*;
 
-public class DraggablePane extends Pane {
+public class DraggablePane extends ZoomingPane {
     private double startX;
     private double startY;
 
@@ -26,19 +28,23 @@ public class DraggablePane extends Pane {
 
         this.setOnMouseDragged(event -> {
             if (isLayoutXValid(event.getSceneX() - startX)) {
-                this.setLayoutX(event.getSceneX() - startX);
+                setLayoutX(event.getSceneX() - startX);
+
             }
             if (isLayoutYValid(event.getSceneY() - startY)) {
-                this.setLayoutY(event.getSceneY() - startY);
+                setLayoutY(event.getSceneY() - startY);
+
             }
         });
 
         this.setOnScroll(event -> {
-            if (isScaleYValid(event.getDeltaY() / 100 + this.getScaleY()) && isScaleXValid(event.getDeltaY() / 100 + this.getScaleX())) {
-//                new ZoomOperator().zoom(this, event.getDeltaY() / 100 + this.getScaleX(), event.getSceneX(), event.getSceneY());
-                this.setScaleX(event.getDeltaY() / 100 + this.getScaleX());
-                this.setScaleY(event.getDeltaY() / 100 + this.getScaleY());
-            }
+//            if (isScaleYValid(event.getDeltaY() / 100 + this.getZoomFactor()) && isScaleXValid(event.getDeltaY() / 100 + this.getZoomFactor())) {
+//                new ZoomOperator().zoom(this, event.getDeltaY() / 100 + this.getScaleX(), event.getScreenX(), event.getScreenY());
+//                this.setScaleX(event.getDeltaY() / 100 + this.getScaleX());
+//                this.setScaleY(event.getDeltaY() / 100 + this.getScaleY());
+                double delta = 1.1;
+                zoomAroundPivot(Math.pow(delta, event.getDeltaY() / 100) , event.getScreenX(), event.getScreenY());
+//            }
         });
 
     }
@@ -60,22 +66,53 @@ public class DraggablePane extends Pane {
     }
 }
 
-class ZoomOperator {
+class ZoomingPane extends Pane {
+    private Translate initialLayout;
+    private Scale scale;
 
-    public ZoomOperator() {
-
+    public ZoomingPane(Node... children) {
+        super(children);
+        scale = new Scale(1, 1);
+        initialLayout = new Translate(getLayoutX(), getLayoutY());
+        scale.setPivotX(getLayoutX());
+        scale.setPivotY(getLayoutY());
+        getTransforms().addAll(scale);
     }
 
-    public void zoom(Node node, double scale, double x, double y) {
-        // determine scale
+//    protected void layoutChildren() {
+//        Pos pos = Pos.TOP_LEFT;
+//        double width = getWidth();
+//        double height = getHeight();
+//        double top = getInsets().getTop();
+//        double right = getInsets().getRight();
+//        double left = getInsets().getLeft();
+//        double bottom = getInsets().getBottom();
+//        double contentWidth = (width - left - right)/zoomFactor.get();
+//        double contentHeight = (height - top - bottom)/zoomFactor.get();
+//        layoutInArea(this, left, top,
+//                contentWidth, contentHeight,
+//                0, null,
+//                pos.getHpos(),
+//                pos.getVpos());
+//    }
 
-        double dx = ((node.getLayoutX() - x) / node.getScaleX()) * scale;
-        double dy = ((node.getLayoutY() - y) / node.getScaleY()) * scale;
 
-        node.setLayoutX(x + dx);
-        node.setLayoutY(y + dy);
-        node.setScaleX(scale);
-        node.setScaleY(scale);
+    final void zoomAroundPivot(Double zoomFactor, double screenX, double screenY) {
+        double prevPivotX = scale.getPivotX();
+        double prevPivotY = scale.getPivotY();
+        double prevScaleX = scale.getX();
+        double prevScaleY = scale.getY();
+        double prevTranslateX = getLayoutX() - initialLayout.getX();
+        double prevTranslateY = getLayoutY() - initialLayout.getY();
+        System.err.println(getLayoutX() + " " + getLayoutY());
+        scale.setPivotX(prevPivotX - prevPivotX/prevScaleX  - prevTranslateX/prevScaleX + screenX/prevScaleX);
+        scale.setPivotY(prevPivotY - prevPivotY/prevScaleY  - prevTranslateY/prevScaleY + screenY/prevScaleY);
+        scale.setX(zoomFactor * prevScaleX);
+        scale.setY(zoomFactor * prevScaleY);
+        setLayoutX(screenX - scale.getPivotX());
+        setLayoutY(screenY - scale.getPivotY());
     }
 
 }
+
+
