aa23f13b8e092322abe03309022b33a6fa41f843
Hamed <amzhamed@gmail.com>
2018-07-07 16:18:12 +0430

    BuildingHolder modified

--- src/viewers/utils/BuildingHolder.java
+++ src/viewers/utils/BuildingHolder.java
@@ -1,56 +1,58 @@
 package viewers.utils;
 
-import javafx.scene.control.ProgressBar;
 import javafx.scene.effect.Glow;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import models.GameLogic.Builder;
 import models.GameLogic.Entities.Buildings.Building;
 
-public class BuildingHolder extends StackPane {
+public class BuildingHolder extends Pane {
     private Builder builder;
     private Building building;
     private ImageView imageView;
-    private ProgressBar progressBar;
+    private ProgressBarItem hitPointsProgressBar;
+    private ProgressBarItem constructionProgressBar;
     private Glow glow = new Glow(0);
-    private boolean isUnderConstruction;
 
     public BuildingHolder(Building building) {
         this.building = building;
-        this.imageView = building.getImageView();
-        this.isUnderConstruction = false;
-        this.setMaxSize(imageView.getViewport().getWidth(), imageView.getViewport().getHeight());
-        this.getChildren().addAll(imageView);
         initialize();
     }
 
     public BuildingHolder(Builder builder) {
         this.builder = builder;
         this.building = builder.getUnderConstructBuilding();
-        this.imageView = building.getImageView();
-        this.isUnderConstruction = true;
-        this.progressBar.setProgress(this.builder.getConstructRemainingTime() / this.builder.getTotalConstructionTime());
-        this.setMaxSize(imageView.getViewport().getWidth(), imageView.getViewport().getHeight());
-        this.getChildren().addAll(imageView, progressBar);
         initialize();
     }
 
     private void initialize() {
+        this.imageView = building.getImageView();
+        this.setMaxSize(imageView.getViewport().getWidth(), imageView.getViewport().getHeight());
+        hitPointsProgressBar = new ProgressBarItem(1,1, ProgressBarType.HIT_POINTS);
+        constructionProgressBar = new ProgressBarItem(1,1, ProgressBarType.REMAINED_TIME);
+        this.getChildren().addAll(imageView, constructionProgressBar, hitPointsProgressBar);
         this.setId("building-holder");
-        progressBar = new ProgressBar();
-        progressBar.setId("building-progress-bar");
-        progressBar.setPrefWidth(Const.BUILDING_TILE_WIDTH);
-        progressBar.setPrefHeight(6);
         imageView.setEffect(glow);
         imageView.setOnMouseEntered(event -> glow.setLevel(0.5));
         imageView.setOnMouseExited(event -> glow.setLevel(0));
     }
 
-    public ImageView getImageView() {
-        return imageView;
-    }
+    public void refresh() {
+        if (building.isUnderConstruct()) {
+            constructionProgressBar.setMax(builder.getTotalConstructionTime());
+            constructionProgressBar.setValue(builder.getTotalConstructionTime() - builder.getConstructRemainingTime());
+            constructionProgressBar.setVisible(true);
+        } else {
+            constructionProgressBar.setVisible(false);
+        }
 
-    public ProgressBar getProgressBar() {
-        return progressBar;
+        if (building.getHitPoints() < building.getMaxHitPoints()) {
+            hitPointsProgressBar.setMax(building.getMaxHitPoints());
+            hitPointsProgressBar.setValue(building.getHitPoints());
+            hitPointsProgressBar.setVisible(true);
+        } else {
+            hitPointsProgressBar.setVisible(false);
+        }
     }
 }
