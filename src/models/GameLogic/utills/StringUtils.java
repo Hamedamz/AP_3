4179d2f1328a8a27d43e761ad43cf655cb508ffc
arshia <arshiamoghimi98@gmail.com>
2018-07-09 22:44:43 +0430

    refactored stringSeparator method.

--- src/models/GameLogic/utills/StringUtils.java
+++ src/models/GameLogic/utills/StringUtils.java
@@ -3,10 +3,10 @@ package models.GameLogic.utills;
 public class StringUtils {
     public static String stringSeparator(String old) {
         String[] split = old.split("(?=\\p{Upper})");
-        String seperated = "";
-        for (String s1 : split) {
-            seperated += s1 + " ";
+        StringBuilder separated = new StringBuilder();
+        for (String s : split) {
+            separated.append(s).append(" ");
         }
-        return seperated.trim();
+        return separated.toString().trim();
     }
 }
