4fecf0e4597434465122734c5debb2167f6fbc00
arshia <arshiamoghimi98@gmail.com>
2018-07-15 18:52:42 +0430

    trap rearm completed

--- src/models/GameLogic/Entities/Buildings/Building.java
+++ src/models/GameLogic/Entities/Buildings/Building.java
@@ -202,6 +202,9 @@ public abstract class Building extends Defender implements Upgradable, Comparabl
         if (hitPoints > maxHitPoint) {
             isDestroyed = false;
             hitPoints = maxHitPoint;
+            if (this instanceof Trap) {
+                ((Trap) this).rearm();
+            }
         }
     }
 
