18d02bf1b668c82d4aa5311b09cb7ce4c320c988
Hamed <amzhamed@gmail.com>
2018-07-17 14:23:59 +0430

    fast forward added arrows improved

--- src/viewers/utils/ArrowShot.java
+++ src/viewers/utils/ArrowShot.java
@@ -8,6 +8,10 @@ import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.util.Duration;
+import models.GameLogic.Entities.Buildings.Trap;
+import models.GameLogic.Entities.Troop.Giant;
+import models.GameLogic.Entities.Troop.Guardian;
+import models.GameLogic.Entities.Troop.WallBreaker;
 import models.interfaces.Attacker;
 import models.interfaces.Destroyable;
 
@@ -20,8 +24,11 @@ public class ArrowShot extends Pane {
     private Circle arrow;
 
     public ArrowShot(Attacker attacker, Destroyable destroyable) {
-        double toX = IsometricPane.getIsometricX(destroyable.getPosition());
-        double toY = IsometricPane.getIsometricY(destroyable.getPosition());
+        if (attacker instanceof Guardian || attacker instanceof Giant || attacker instanceof WallBreaker || attacker instanceof Trap) {
+            return;
+        }
+        double toX = IsometricPane.getIsometricX(destroyable.getPosition()) + Const.ENTITY_TILE_WIDTH / 8;
+        double toY = IsometricPane.getIsometricY(destroyable.getPosition()) + Const.ENTITY_TILE_HEIGHT / 8;
         double fromX = IsometricPane.getIsometricX(attacker.getPosition());
         double fromY = IsometricPane.getIsometricY(attacker.getPosition());
 
@@ -36,11 +43,11 @@ public class ArrowShot extends Pane {
         this.setPrefWidth(width);
         this.setHeight(height);
 
-        arrow = new Circle(fromX - layoutX, fromY - layoutY, 6, Color.CORNSILK);
+        arrow = new Circle(0, 0, 6, Color.CORNSILK);
         this.getChildren().add(arrow);
 
-        MoveTo moveTo = new MoveTo(0, 0);
-        LineTo lineTo = new LineTo(toX - fromX, toY - fromY);
+        MoveTo moveTo = new MoveTo(fromX - layoutX, fromY - layoutY);
+        LineTo lineTo = new LineTo(toX - layoutX, toY - layoutY);
         Path path = new Path(moveTo, lineTo);
         PathTransition pathTransition = new PathTransition(new Duration(500), path);
         pathTransition.setNode(arrow);
